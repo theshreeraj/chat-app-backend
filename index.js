8aaf734ba62f2f2f096aba002688cdf16a059610
theshreeraj <shreerajmane007@gmail.com>
2025-01-07 16:07:38 +0530

    minor change

--- index.js
+++ index.js
@@ -1,41 +1,12 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const express = require('express');
 const app = express();
+const port = 3000;
 
-const port = 8080;
 
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
-
-// Define User schema and model
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-});
-
-const User = mongoose.model("User", userSchema);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/getAllUsers", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json({ data: users });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
